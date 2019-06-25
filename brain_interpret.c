c69ebef10ac2c2693806a11af6d07a7a0684b7cb
ZacharyRJohnson <zachjohnson1900@gmail.com>
2019-06-24 20:37:46 -0400

    started implementing interpretation

--- brain_interpret.c
+++ brain_interpret.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 #define MAX_LINE 256
+#define VIRTUAL_MEM_SIZE 1024
+
 /*
  * Reads a single line of code from stdin
  */
@@ -15,12 +17,55 @@ char* read_line()
     return line;
 }
 
+void execute(char* line)
+{
+    int ptr = 0;
+    int memory[VIRTUAL_MEM_SIZE] = {0};
+
+    for(int i=0; i < VIRTUAL_MEM_SIZE; i++){
+        char symbol = line[i];
+        switch(symbol)
+        {
+            case '<':
+                if(ptr == 0)
+                    ptr = VIRTUAL_MEM_SIZE - 1;
+                else
+                    ptr--;
+                break;
+
+            case '>':
+                if(ptr == VIRTUAL_MEM_SIZE-1)
+                    ptr = 0;
+                else
+                    ptr++;
+                break;
+
+            case '+':
+                memory[ptr]++;
+                break;
+
+            case '-':
+                memory[ptr]--;
+                break;
+
+            case '.':
+                printf("%d\n", memory[ptr]);
+                break;
+
+            case ',':
+                fgets(memory[ptr], 1, stdin);
+                break;
+
+        }
+    }
+}
+
 int main()
 {
     char *line;
     line = read_line();
     printf("%s", line);
-
+    execute(line);
     free(line);
     return(0);
 }
